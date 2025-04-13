4a000277d98261cf18d899e082e37e1ec86402a1
Sixaven <2985855672@qq.com>
2025-04-13 21:28:34 +0800

    add启动界面跳转

---
+++ entry/src/main/ets/common/string/String.ts
@@ -0,0 +1,9 @@
+
+export class launcher_string{
+  static Dialog_launcher_title ="欢迎使用智行知APP";
+  static Dialog_launcher_title_content ="我们充分尊重用户的隐私权，并按照法律要求和业界成熟的安全标准，为您的个人信息提供相应的安全保护措施。";
+  static Dialog_launcher_privacy ="隐私安全保护声明";
+  static Dialog_launcher_privacy_content ="（以下简称\"本声明\"）以便您了解我们如何搜集、使用、披露、保护、存储以及传输您的个人数据。请你仔细阅读本声明。如您有任何疑问，请告诉我们。";
+  static Button_yes = "同意";
+  static Button_no = "不同意";
+}
